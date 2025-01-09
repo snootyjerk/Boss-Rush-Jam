5c1eedf4cbdb4b2e54f2184b36c20442a6843d17
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-01-09 17:47:19 -0600

    add plug and outlet with physics

---
+++ outlet.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+
+
+@onready var plug_body: RigidBody2D = $Plug/Body
+@onready var joint: PinJoint2D = $Plug/PinJoint2D
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("UNPLUGGDED")
+	joint.node_b = ""
+	
+	plug_body.gravity_scale = 1.0
+	
+	for child in $Plug.get_children():
+		if child is CableSegment:
+			child.gravity_scale = 1.0
