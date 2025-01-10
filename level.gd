04d4e4ac78d8ab415b0f441a4003744858333e89
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-01-10 09:50:35 -0600

    add boss music

--- level.gd
+++ level.gd
@@ -3,16 +3,27 @@ class_name Level
 
 const LEVEL_DIR: String = "res://Levels/level_"
 @export var _next_level_name: String
+@onready var boss_music: AudioStreamPlayer = $BossMusic
+
 
 func get_next_level_path() -> String:
 	return LEVEL_DIR + _next_level_name + ".tscn"
+
 
 var _is_boss_defeated: bool = false
 var is_boss_defeated: bool:
 	get:
 		return _is_boss_defeated
+	set(defeated):
+		_is_boss_defeated = defeated
+		if defeated:
+			boss_music.stop()
 
 
 # make invisible for level transition
 func _exit_tree():
 	visible = false
+
+
+func _on_outlet_unplugged() -> void:
+	is_boss_defeated = true
--- outlet.gd
+++ outlet.gd
@@ -1,12 +1,12 @@
 extends Sprite2D
 
+signal unplugged
 
 @onready var plug_body: RigidBody2D = $Plug/Body
 @onready var joint: PinJoint2D = $Plug/PinJoint2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("UNPLUGGDED")
 	joint.node_b = ""
 	
 	plug_body.gravity_scale = 1.0
@@ -14,3 +14,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	for child in $Plug.get_children():
 		if child is CableSegment:
 			child.gravity_scale = 1.0
+			
+	unplugged.emit()
