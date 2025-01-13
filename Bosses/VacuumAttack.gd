a94057ec99eb9284a8b71932898e31842c0b33e4
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-01-13 10:38:10 -0600

    adjustments to suction vortex parameters, add delta

--- Bosses/VacuumAttack.gd
+++ Bosses/VacuumAttack.gd
@@ -12,10 +12,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	rotation -= _rotation_speed
+func _physics_process(delta: float) -> void:
+	rotation -= _rotation_speed * delta
 	for target in _target_list:
-		target._add_velocity(target.global_position.direction_to(global_position) * _power)
+		target._add_velocity(target.global_position.direction_to(global_position) * _power * delta)
 	
 func _set_enabled(enabled: bool):
 	_active = enabled
