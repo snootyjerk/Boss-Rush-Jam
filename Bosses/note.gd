7dce776dcd97996bcb9a5d5673ff0218c854fbc6
snootyjerk <87242147+snootyjerk@users.noreply.github.com>
2025-01-20 20:10:31 -0500

    Added Cello boss sprites
    Added the Cello, Dancer, Note, and Bow sprites, and attached them to objects. Created the note as a projectile that moves in a wave form.

---
+++ Bosses/note.gd
@@ -0,0 +1,21 @@
+extends RigidBody2D
+
+var velocity = Vector2(-1,0)
+@export var _amplitude = 1
+@export var _frequency = 5
+@export var _max_time = 5
+
+var _timer = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	_timer+= 1*delta
+	
+	velocity.y = sin(_timer*_frequency) * _amplitude
+	global_position += velocity
+	if _timer <= 0:
+		queue_free()
