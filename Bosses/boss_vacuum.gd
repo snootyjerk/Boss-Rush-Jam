6c067bbaeda42d81ce6f29e21f0d20e280fa2f28
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-01-23 17:54:43 -0600

    vacuum jumps at shorter intervals as phase increases

--- Bosses/boss_vacuum.gd
+++ Bosses/boss_vacuum.gd
@@ -3,13 +3,17 @@ class_name BossVacuum
 
 var _boss_death_head = preload("res://Bosses/vacuum_boss_death_head.tscn")
 
+@export var _jump_delay_phase_2: float = 7.5
+@export var _jump_delay_phase_3: float = 6.0
+@export var _jump_delay_phase_4: float = 4.0
+
 @onready var _head: VacuumHead = $HeadCharacter2D
 
 @onready var _jump_starting_point = global_position
 @onready var _jump_points = [Vector2(61,88),_jump_starting_point,Vector2(567,168)]
 var _jump_direction = 1
 
-var _jumping = true
+var _jumping = false
 var _can_jump = true
 var _current_point = 1
 var _next_point = _current_point + _jump_direction
@@ -18,6 +22,10 @@ var _jump_arc_height = -200
 var _jump_process = 0.0
 var _action_timer_max = 3
 var _action_timer = _action_timer_max
+
+
+func _ready() -> void:
+	Main.node.boss_phase_changed.connect(_on_phase_changed)
 
 
 func _boss_defeated_hook():
@@ -43,7 +51,7 @@ func _process(delta: float) -> void:
 			_jump_starting_point = _jump_points[_current_point]
 			_head._tether_point = global_position
 			_jump_process = 0
-	else:
+	if Main.node.boss_phase > 1:
 		_action_timer -= 1*delta
 		if _action_timer <= 0:
 			_action_timer = _action_timer_max
@@ -55,3 +63,12 @@ func _jump_movement(endpoint: Vector2):
 	var curve_point0 = _jump_starting_point.lerp(jump_arc_point,_jump_process)
 	var curve_point1 = jump_arc_point.lerp(endpoint,_jump_process)
 	global_position = curve_point0.lerp(curve_point1,_jump_process)
+	
+	
+func _on_phase_changed(new_phase: int):
+	if new_phase == 2:
+		_action_timer_max = _jump_delay_phase_2
+	elif new_phase == 3:
+		_action_timer_max = _jump_delay_phase_3
+	elif new_phase == 4:
+		_action_timer_max = _jump_delay_phase_4
