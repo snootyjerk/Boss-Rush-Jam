9cbd74f77123f1a9c397249e71c9bdbd4d2e5364
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-01-30 15:02:01 -0600

    fix objects that weren't pausable including timer

--- time_label.gd
+++ time_label.gd
@@ -1,20 +1,22 @@
 extends Label
 
 
-var _init_time_ms: int
-var _current_time_ms: int
+#var _init_time_ms: int
+#var _current_time_ms: int
+
+var _time_passed: int = 0
 
 
-func _ready() -> void:
-	_init_time_ms = Time.get_ticks_msec()
-	_current_time_ms = Time.get_ticks_msec() - _init_time_ms
+#func _ready() -> void:
+	#_init_time_ms = Time.get_ticks_msec()
+	#_current_time_ms = Time.get_ticks_msec() - _init_time_ms
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	_current_time_ms = Time.get_ticks_msec() - _init_time_ms
-	var seconds = (_current_time_ms / 1000) % 60
-	var minutes = (_current_time_ms / 1000) / 60
+	_time_passed += round(delta * 1000)
+	var seconds = (_time_passed / 1000) % 60
+	var minutes = (_time_passed / 1000) / 60
 	
 	var str_seconds = str(seconds)
 	if seconds < 10:
