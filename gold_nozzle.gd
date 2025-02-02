67870ac021ed2b348ff90fb05527dc12888e82e1
Pixels <162462529+PixelsToPrevail@users.noreply.github.com>
2025-02-02 10:52:48 -0600

    gold nozzle invisible until boss defeated

--- gold_nozzle.gd
+++ gold_nozzle.gd
@@ -4,7 +4,7 @@ var _seed = preload("res://Levels/seed.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_on_washer_level_boss_defeated()
+	visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,4 +16,5 @@ func _on_animated_sprite_2d_animation_finished() -> void:
 	new_seed.global_position = Vector2(global_position.x,global_position.y+15)
 
 func _on_washer_level_boss_defeated() -> void:
+	visible = true
 	$AnimatedSprite2D.play()
